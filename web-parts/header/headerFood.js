68d753e0ebbfe2f9afbf50faa603885feb980e20
aurialt <144707111+aurialt@users.noreply.github.com>
2025-02-01 21:59:14 -0600

    Made everything dynamic and modular

--- web-parts/header/headerFood.js
+++ web-parts/header/headerFood.js
@@ -8,22 +8,22 @@ function loadHeader() {
     // Load the CSS dynamically
     const link = document.createElement("link");
     link.rel = "stylesheet";
-    link.href = "../webmaster2025/web-parts/header/header.css" // Path to the CSS file
+    link.href = "../../webmaster2025/web-parts/header/header.css" // Path to the CSS file
     document.head.appendChild(link);
 
     console.log("LoadHeader Function trying to do it's job");
     // Return the footer HTML structure
     return `
     <header class="header">
-        <img src="images/nectar-removebg-preview.png" alt="Logo" id="homelogo" width="65px" onclick="location.href='index.html'">
+        <img src="../images/nectar-removebg-preview.png" alt="Logo" id="homelogo" width="65px" onclick="location.href='../index.html'">
         <nav class="navbar">
-            <a href="cart.html" id="cart-label">Cart</a>
-            <a href="index.html">Home</a>
-            <a href="menu.html">Menu</a>
-            <a href="login.html">Order</a>
-            <a href="about.html">About</a>
-            <a href="contact.html">Contact</a>
-            <a href="reference.html">Reference Page</a>
+            <a href="../cart.html" id="cart-label">Cart</a>
+            <a href="../index.html">Home</a>
+            <a href="../menu.html">Menu</a>
+            <a href="../login.html">Order</a>
+            <a href="../about.html">About</a>
+            <a href="../contact.html">Contact</a>
+            <a href="../reference.html">Reference Page</a>
         </nav>
     </header>
     `;
