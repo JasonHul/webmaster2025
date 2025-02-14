96d1a1638b38a1988d208a00a1569791a5cc0c26
JasonHul <jh1036079@student.francistuttle.edu>
2025-02-14 08:13:23 -0600

    added the loadscript to all pages instead of the gloabaljs

--- js/global.js
+++ js/global.js
@@ -42,4 +42,3 @@ function isMobile() {
 let message = isMobile() ? "You are using a mobile device. (Just A test)" : "You are using a desktop browser. (Just a test)";
 document.getElementById("deviceType").textContent = message;
 
-<script src="//instant.page/5.2.0" type="module" integrity="sha384-jnZyxPjiipYXnSU0ygqeac2q7CVYMbh84q0uHVRRxEtvFPiQYbXWUorga2aqZJ0z"></script>
