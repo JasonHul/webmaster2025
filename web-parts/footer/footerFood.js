9873c14c19aafeabb2035d77750eab181fa55afd
aurialt <144707111+aurialt@users.noreply.github.com>
2025-02-22 15:43:00 -0600

    fixed footer issues

--- web-parts/footer/footerFood.js
+++ web-parts/footer/footerFood.js
@@ -11,18 +11,16 @@ function loadFooter() {
     console.log("Load Function trying to do it's job");
     // Return the footer HTML structure
     return `
-    <footer>
-        <div class="footer-content">
-            <img src="../images/nectar-removebg-preview.png" width="5%" alt="Logo" id="footer-logo" onclick="location.href='../index.html'">
-            <nav class="footer-links">
-                <a href="../index.html">Home</a>
-                <a href="../menu.html">Menu</a>
-                <a href="../food/specials.html">Specials</a>
-                <a href="../about.html">About</a>
-                <a href="../contact.html">Contact</a>
-            </nav>
-            <p class="copyright">&copy; 2025 Nectar. All Rights Reserved.</p>
-        </div>
+    <footer class="footer-content">
+        <img src="../images/toWEBP/nectar-removebg-preview.webp" width="5%" alt="Logo" id="footer-logo" onclick="location.href='../index.html'">
+        <nav class="footer-links">
+            <a href="../index.html">Home</a>
+            <a href="../menu.html">Menu</a>
+            <a href="../food/specials.html">Specials</a>
+            <a href="../about.html">About</a>
+            <a href="../contact.html">Contact</a>
+        </nav>
+        <p class="copyright">&copy; 2025 Nectar. All Rights Reserved.</p>
     </footer>
     `;
     }
